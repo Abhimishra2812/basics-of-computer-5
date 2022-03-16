0bfdbb5b984bbcf24307b64940b4cdc9f60aba33
Abhimishra2812 <101655063+Abhimishra2812@users.noreply.github.com>
2022-03-16 23:25:35 +0530

    Add files via upload

---
+++ basics of computer 5.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+get_ipython().set_next_input('1 )What is storage management and what are the advantages of storage management');get_ipython().run_line_magic('pinfo', 'management')
+Ans:A storage management strategy makes it easier for administrators to plan and maintain their storage systems.
+    With the right strategy, they can provide their apps and users with the storage capacity they need,
+    while avoiding unused or overprovisioned storage.
+    
+get_ipython().set_next_input('2 )How are I/O operations performed');get_ipython().run_line_magic('pinfo', 'performed')
+Ans:Like any other activity in a computer system, I/O is a concerted work of both hardware and software.
+    The software which is executed to carry out an I/O transaction for a specific I/O device is called a device driver.
+    An example of such an I/O transaction is reading a block of data from disk to memory.
+    
+get_ipython().set_next_input('3 )What is a bit');get_ipython().run_line_magic('pinfo', 'bit')
+Ans:A bit (short for binary digit) is the smallest unit of data in a computer. 
+    
+get_ipython().set_next_input('4 )What do you understand about computer storage');get_ipython().run_line_magic('pinfo', 'storage')
+Ans:Storage is a process through which digital data is saved within a data storage device by means of computing technology.
+    Storage is a mechanism that enables a computer to retain data, either temporarily or permanently.
+    
+get_ipython().set_next_input('5 )What is KB? How many bytes is 1 KB');get_ipython().run_line_magic('pinfo', 'KB')
+Ans:A kilobyte (KB) is a multiple unit used for binary data.One kilobyte (KB) is a collection of about 1000 bytes.
+    
+get_ipython().set_next_input('6 )What is TB and how many bytes is 1 TB');get_ipython().run_line_magic('pinfo', 'TB')
+Ans:A terabyte (TB) is a unit of digital data that is equal to about 1 trillion bytes.
+    
+7 )What is Microsoft Office? List its products.
+Ans:Microsoft Office is a suite of applications designed to help with productivity and completing common tasks on a computer. 
+    The products of MS office is
+    .Word.
+    .Excel.
+    .PowerPoint.   
+    
+get_ipython().set_next_input('8 )Write the steps to create and save a word file. What is the file extension of a word file');get_ipython().run_line_magic('pinfo', 'file')
+Ans:The steps to create and save a word file are
+    .Click FILE > Save, pick or browse to a folder, type a name for your document in the File name box, and click Save.
+    .Save your work as you go - hit Ctrl+S often.
+    .To print, click the FILE tab, and then click Print.
+
